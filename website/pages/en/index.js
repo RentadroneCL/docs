dc1e449802852ecf95ad7a1ca88eca479d4a3249
Alvaro Farias <alvaro.farias@protonmail.com>
2020-04-26 14:59:02 -0400

    :art: update page

--- website/pages/en/index.js
+++ website/pages/en/index.js
@@ -6,6 +6,25 @@ const Container = CompLibrary.Container;
 const GridBlock = CompLibrary.GridBlock;
 
 function Welcome(props) {
+  const {config: siteConfig, language = ''} = props;
+  const {baseUrl, docsUrl} = siteConfig;
+  const docsPart = `${docsUrl ? `${docsUrl}/` : ''}`;
+  const langPart = `${language ? `${language}/` : ''}`;
+  const docUrl = doc => `${baseUrl}${docsPart}${langPart}${doc}`;
+
+  const links = [
+    {
+      content: `Learn more using the [documentation on this site.](${docUrl(
+        'detection-models.html',
+      )})`,
+      title: 'Browse Docs',
+    },
+    {
+      content: 'Ask questions about the documentation and project <a href="https://join.slack.com/t/rentadronecl/shared_invite/zt-dvgh8er7-hsv4p_jf75dsgkyGE1dHuw">Join Slack</a>',
+      title: 'Join the community',
+    }
+  ];
+
   return (
     <div className="docMainWrapper wrapper">
       <Container className="mainContainer documentContainer postContainer">
@@ -18,6 +37,7 @@ function Welcome(props) {
           A part of our workload is dedicated to open source and side projects.
           You can spot team members in user groups or as conference speakers as well.
         </p>
+        <GridBlock contents={links} layout="twoColumn" />
       </Container>
     </div>
   );
