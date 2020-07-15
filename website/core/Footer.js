9d9624aee466f35e842d5f63a326380396de5aad
Alvaro Farias <alvaro.farias@protonmail.com>
2020-07-15 19:37:30 -0400

    :art: update footer

--- website/core/Footer.js
+++ website/core/Footer.js
@@ -37,7 +37,7 @@ class Footer extends React.Component {
           </a>
           <div>
             <h5>Docs</h5>
-            <a href={this.docUrl('detection-models', this.props.language)}>
+            <a href={this.docUrl('developers', this.props.language)}>
               Getting Started
             </a>
             {/* <a href={this.docUrl('doc2.html', this.props.language)}>
